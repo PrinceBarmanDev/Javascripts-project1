f42cab570e46c9c12f1323fe5b1d09930bcbfd59
PrinceBarmanDev <princebarman7.cse@gmail.com>
2026-09-22 17:22:16 +0530

    remove files from  repository

--- student-info.js
+++
@@ -1,8 +0,0 @@
-function introduce(name, age, course) {
-    console.log("Hello!");
-    console.log("My name is " + name);
-    console.log("I am " + age + " years old");
-    console.log("I am studying " + course);
-}
-
-introduce("Prince", 18, "B.Tech CSE");
