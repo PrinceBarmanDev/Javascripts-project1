19fc4869f352c7a3894f38f0edb5f9515acb24da
PrinceBarmanDev <princebarman7.cse@gmail.com>
2026-09-22 16:54:14 +0530

    initial

--- 1_std_info.js
+++ 1_std_info.js
@@ -5,6 +5,10 @@ var college_name="SITM";
 let marks=82;
 let birth_year=2008;
 
+let city="Guwahati";
+let semester="1st Semester";
+let favourite_subject="Mathematics";
+
 age=21;
 marks=85;
 
@@ -14,8 +18,9 @@ console.log("Course :",cousre);
 console.log("College Name :",college_name);
 console.log("Marks Obtained :",marks);
 console.log("Date of Birth :",birth_year);
-
-
+console.log("City :",city);
+console.log("Semester :",semester);
+console.log("Favourite subject :",favourite_subject);
 
 
 
