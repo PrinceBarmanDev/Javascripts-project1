ee5c14a3ec339416b1886398e4cad9dfba065592
PrinceBarmanDev <princebarman7.cse@gmail.com>
2026-09-22 16:47:59 +0530

    initial

---
+++ 1_std_info.js
@@ -0,0 +1,23 @@
+let student_name="Prince Barman";
+let age=18;
+var cousre="B.Tech CSE";
+var college_name="SITM";
+let marks=82;
+let birth_year=2008;
+
+age=21;
+marks=85;
+
+console.log("Student Name :",student_name);
+console.log("Age :",age);
+console.log("Course :",cousre);
+console.log("College Name :",college_name);
+console.log("Marks Obtained :",marks);
+console.log("Date of Birth :",birth_year);
+
+
+
+
+
+
+
